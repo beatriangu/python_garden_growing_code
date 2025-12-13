ebc804635b5a801b3df8c33bd79ad39b9f79a9c6
Beatriz Lamiquiz <147153901+beatriangu@users.noreply.github.com>
2025-12-13 11:49:37 +0100

    conex3

---
+++ ex2/ft_harvest_total.py
@@ -0,0 +1,3 @@
+def ft_harvest_total(harvest1, harvest2, harvest3):
+    total = harvest1 + harvest2 + harvest3
+    print(f"Total harvest: {total}")
---
+++ ex3/ft_plant_age.py
@@ -0,0 +1,6 @@
+def ft_plant_age(age_days):
+    if age_days > 60:
+        print("The plant is ready for harvest.")
+    else:
+        print("The plant is still growing.")
+
